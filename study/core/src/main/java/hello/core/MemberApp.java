b2f5eff1d12dc98838b68d74c916ad9afc0cef20
Eunjin.Kim <jennyeunjin@khu.ac.kr>
2023-08-02 16:25:50 +0900

    Lecture Section3

--- study/core/src/main/java/hello/core/MemberApp.java
+++ study/core/src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,15 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // AppConfig의 설정 정보를 가지고 스프링 빈 객체들을 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 스플이 빈에서 이름이 memberService인 객체를 꺼낸다
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
