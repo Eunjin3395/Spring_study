2f6014d76932cd163326eddbd313504940418c1e
Eunjin.Kim <jennyeunjin@khu.ac.kr>
2023-08-09 14:55:28 +0900

    Lecture Section6

---
+++ study/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class) //컴포넌트 어노테이션 붙은건 다 자동으로 스프링 빈 등록 해주는데 얘는 제외한다
+)
+public class AutoAppConfig {
+
+    @Bean(name="memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
